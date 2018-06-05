bbd4ee6a4e118392c9a1abac8cfe5176698860e5
waldinho <james.waller1984@gmail.com>
2018-06-06 09:25:32 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# -react-tesla-range-calculator-redux
+react-tesla-range-calculator
+
+Project URL - http://gigantic-yak.surge.sh/
+
+To do: incorperate Redux 
---
+++ src/components/TeslaCounter/TeslaCounter.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './TeslaCounter.css';
+
+const TeslaCounter = ({ initValues, currentValue, increment, decrement }) => (
+  <div className="tesla-counter">
+    <p className="tesla-counter__title">{initValues.title}</p>
+    <div className="tesla-counter__container cf">
+      <div className="tesla-counter__item">
+        <p className="tesla-counter__number">
+          { currentValue }
+          <span>{ initValues.unit }</span>
+        </p>
+        <div className="tesla-counter__controls">
+          <button 
+            onClick={(e) => {
+              e.preventDefault();
+              increment(currentValue)}}
+            disabled={currentValue >= initValues.max} 
+          >
+          </button>
+          <button 
+            onClick={(e) => {
+              e.preventDefault();
+              decrement(currentValue)}}
+            disabled={currentValue <= initValues.min} 
+          >
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>  
+);
+
+TeslaCounter.propTypes = {
+  currentValue: PropTypes.number,
+  increment: PropTypes.func,
+  decrement: PropTypes.func,
+  initValues: PropTypes.object
+}
+
+export default TeslaCounter;
